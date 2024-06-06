1583dcf0bda05767f94415597264978c07956e6b
Takkeshi <prosaso12133@gmail.com>
2024-06-06 13:15:12 -0600

    chore: Update long description in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='Takkeshi',
     author_email='tacomastabusiness@gmail.com',
     description='Python library designed to create customizable overlays for desktop applications. It uses PyQt5 to draw shapes such as circles, rectangles, and lines on a transparent window that stays on top of other windows.',
-    long_description=long_description,  # Add this line
+    long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),    
     install_requires=['PyQt5 >= 5.15.4'],
