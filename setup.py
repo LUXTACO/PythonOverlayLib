04435c5d6ad388db52e255b06fe675b2ae4e7227
Takkeshi <prosaso12133@gmail.com>
2024-06-06 13:30:05 -0600

    chore: Update PythonOverlayLib version to 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='PythonOverlayLib',
-    version='1.0.1',
+    version='1.0.2',
     url='https://github.com/LUXTACO/PythonOverlayLib',
     author='Takkeshi',
     author_email='tacomastabusiness@gmail.com',
