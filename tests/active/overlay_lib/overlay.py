662dbbfaa931fc448b0e33f1dab8f0641774794d
Takkeshi <prosaso12133@gmail.com>
2024-06-06 15:35:25 -0600

    chore: Add ChildWindow class for creating a child window with a button and label (support for gui rendering)

--- tests/active/overlay_lib/overlay.py
+++ tests/active/overlay_lib/overlay.py
@@ -8,14 +8,33 @@ from PyQt5.QtGui import QPainter, QPen, QColor
 
 user32 = ctypes.WinDLL('user32.dll')
 
+class ChildWindow(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(ChildWindow, self).__init__(parent)
+
+        # Set the window properties
+        self.setGeometry(50, 50, 200, 200)
+
+        # Create a button in the child window
+        self.button = QtWidgets.QPushButton('Click me', self)
+        self.button.move(50, 50)
+
+        # Create a label in the child window
+        self.label = QtWidgets.QLabel('Hello, world!', self)
+        self.label.move(100, 100)
+
 class MainWindow(QtWidgets.QMainWindow):
     
-    def __init__(self, parent=None, customSystemMetrics:list=None, drawlistCallback:Callable=None, refreshTimeout:int=1):
+    def __init__(self, parent=None, customSystemMetrics:list=None, drawlistCallback:Callable=None, guiCallback:Callable=None, refreshTimeout:int=1):
         super(MainWindow, self).__init__(parent)
         self.setWindowTitle("Overlay")
         self.setGeometry(0, 0, (user32.GetSystemMetrics(0) if not customSystemMetrics else customSystemMetrics[0]), (user32.GetSystemMetrics(1) if not customSystemMetrics else customSystemMetrics[1]))
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowTransparentForInput | QtCore.Qt.WindowStaysOnTopHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        
+        if guiCallback:
+            self.guiCallback = guiCallback
+            self.guiCallback()
         
         self.drawlist = []
         self.drawlistCallback = drawlistCallback
@@ -44,7 +63,7 @@ class MainWindow(QtWidgets.QMainWindow):
                 painter.setFont(QtGui.QFont(item.font, item.size))
                 painter.drawText(item.coords.x, item.coords.y, item.text)
         painter.end()
-        
+ 
 class Overlay:
     
     def __init__(self, customSystemMetrics=None, drawlistCallback:Callable=None, refreshTimeout:int=1): 
