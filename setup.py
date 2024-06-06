c515e6e03b5e1310abb5c25e77ce2d9c9a9b97b1
Takkeshi <prosaso12133@gmail.com>
2024-06-06 13:05:46 -0600

    chore: Update long description in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='PythonOverlayLib',
     version='1.0.0',
@@ -7,6 +10,7 @@ setup(
     author='Takkeshi',
     author_email='tacomastabusiness@gmail.com',
     description='Python library designed to create customizable overlays for desktop applications. It uses PyQt5 to draw shapes such as circles, rectangles, and lines on a transparent window that stays on top of other windows.',
-    packages=find_packages(),    
+    long_description_content_type='text/markdown',
+    ackages=find_packages(),    
     install_requires=['PyQt5 >= 5.15.4'],
 )
