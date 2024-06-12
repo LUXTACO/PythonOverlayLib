2026de8b6151bdb843bd8a898b13f2b02be44bb9
Takkeshi <prosaso12133@gmail.com>
2024-06-12 14:30:44 -0600

    Refactor ChildWindow initialization and add header with title for clarification and stuff

--- tests/active/addgui.py
+++ tests/active/addgui.py
@@ -1,3 +1,4 @@
+import sys
 import overlay_lib
 from overlay_lib import *
 from PyQt5 import QtWidgets, QtCore
@@ -6,12 +7,22 @@ class ChildWindow(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super(ChildWindow, self).__init__(parent)
         
+        self.isShowing = True
+        
         self.setWindowTitle("GuiTest")
         self.setGeometry(0, 0, 1000, 600)
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
         self.setStyleSheet("background-color: rgba(255, 255, 255, 10);")
         
-        self.isShowing = True
+        self.header = QtWidgets.QFrame(self)
+        self.header.setGeometry(0, 0, 1000, 50)
+        self.header.setStyleSheet("background-color: rgba(0, 0, 0, 100);")
+        
+        self.title = QtWidgets.QLabel(self.header)
+        self.title.setGeometry(0, 0, 1000, 50)
+        self.title.setText("GuiTest")
+        self.title.setAlignment(QtCore.Qt.AlignCenter)
+        self.title.setStyleSheet("color: white; font-size: 20px;")
     
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_Insert and self.isShowing:
